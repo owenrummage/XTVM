97909bcef4db77800a6d0f0a4fd80c0076b223cd
charmines <47259933+ilikestohack@users.noreply.github.com>
2025-05-27 00:00:32 -0400

    Fix voicemeeter connection issue and add change event emitter

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,55 @@ import url from "url";
 import { Voicemeeter } from "voicemeeter-connector";
 import XTouchControl from "xtouch-control";
 import { BaseLayer } from "./globals";
+import { EventEmitter } from "stream";
 
 let controller: XTouchControl;
 let vm: Voicemeeter;
 
-// Make me a function that will get all files in the src/layers directory and import them one by one and console.log their defaut export
 
 let activeLayer: BaseLayer;
+
+// Attach exit handler to close the voicemeeter Dll/API
+function attachExitHandler(vm: Voicemeeter) {
+	process.stdin.resume(); // so the program will not close instantly
+
+	function exitHandler(options: { cleanup?: boolean; exit?: boolean }, exitCode: number) {
+		try {
+			vm.disconnect();
+		} catch {
+			console.log("Voicemeeter Disconnect Error Suppressed");
+		}
+		if (options.cleanup) console.log("clean");
+		if (exitCode || exitCode === 0) console.log(exitCode);
+		if (options.exit) process.exit();
+	}
+
+	// do something when app is closing
+	process.on("exit", exitHandler.bind(null, { cleanup: true }));
+
+	// catches ctrl+c event
+	process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+
+	// catches "kill pid" (for example: nodemon restart)
+	process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
+	process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
+
+	// catches uncaught exceptions
+	process.on("uncaughtException", exitHandler.bind(null, { exit: true }));
+}
+
+export const vmEventEmitter = new EventEmitter();
 
 async function run() {
 	controller = new XTouchControl();
 	vm = await Voicemeeter.init();
 
 	vm.connect();
+	attachExitHandler(vm);
+
+	vm.attachChangeEvent(() => {
+		vmEventEmitter.emit("change");
+	});
 
 	const layersDir = path.resolve(__dirname, "./layers");
 	const layerFiles = fs.readdirSync(layersDir).filter((file) => file.endsWith(".ts"));
