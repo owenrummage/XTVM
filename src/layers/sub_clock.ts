3280bebdadc61745693eaabcf239e0d6b8885845
charmines <47259933+ilikestohack@users.noreply.github.com>
2025-05-27 22:22:04 -0400

    Add the letter for AM and PM to clock

--- src/layers/sub_clock.ts
+++ src/layers/sub_clock.ts
@@ -5,16 +5,21 @@ import dayjs from "dayjs";
 
 function setTime() {
 	const currentTime = dayjs();
+	let timeSection = "A";
 	let hour = currentTime.hour();
 	const minute = currentTime.minute();
 	const second = currentTime.second();
 
-	if (hour > 12) hour -= 12;
+	if (hour > 12) {
+		hour -= 12;
+		timeSection = "P";
+	}
 
 	const hourText = hour.toString().padStart(2, "0").split("");
 	const minuteText = minute.toString().padStart(2, "0").split("");
 	const secondText = second.toString().padStart(2, "0").split("");
 
+	controller.right().setTimecodeDisplay(2, timeSection); // A/P
 	controller.right().setTimecodeDisplay(3, secondText[1]); // Seconds 2
 	controller.right().setTimecodeDisplay(4, secondText[0]); // Seconds 1
 	controller.right().setTimecodeDisplay(5, minuteText[1], true); // Minutes 2
