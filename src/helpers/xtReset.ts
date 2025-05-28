4686cf8dede1ed375277af09a8e3d25878982716
charmines <47259933+ilikestohack@users.noreply.github.com>
2025-05-27 22:20:14 -0400

    Reset layer buttons and bus selection

--- src/helpers/xtReset.ts
+++ src/helpers/xtReset.ts
@@ -1,4 +1,5 @@
 import { controller } from "..";
+import { selectBus } from "./voicemeeter/helpers";
 
 export function reset() {
 	for (let i = 1; i < 9; i++) {
@@ -9,6 +10,13 @@ export function reset() {
 		controller.channel(i).setFader(0);
 	}
 	controller.channel(9).setFader(0);
+
+	selectBus();
+
+	controller.right().setControlButton("Inputs", "OFF");
+	controller.right().setControlButton("Busses", "OFF");
+	controller.right().setControlButton("Outputs", "OFF");
+	controller.right().setControlButton("User", "OFF");
 
 	controller.screens().setScreens("");
 }
