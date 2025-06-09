f068d7a8accb93bc2c64e35a47ba8c9bf55f023a
charmines <47259933+ilikestohack@users.noreply.github.com>
2025-06-09 02:04:39 -0400

    Add selectable strips to outputs

--- src/layers/outputs.ts
+++ src/layers/outputs.ts
@@ -11,24 +11,53 @@ import {
 import { clearTopScreens } from "../helpers/xtctlHelper";
 import { muteChannelActionListener, setMuteButtonLeds } from "../helpers/voicemeeter/mute";
 import { vuMeterBusesTask } from "../helpers/voicemeeter/vuMeters";
+import { ControlType } from "xtouch-control";
+import { buildSendListener, setSends } from "../helpers/voicemeeter/sends";
+import { setTopScreenVMLabel } from "../helpers/voicemeeter/topScreenLabel";
+
+let selectedVMChannel = 0;
 
 function refreshFromVM() {
 	setMuteButtonLeds();
 	clearTopScreens();
 	setBottomLabelLCDs();
+	setTopScreenVMLabel(FADER_TYPES.STRIP, selectedVMChannel);
+	setSends(FADER_TYPES.BUS, selectedVMChannel);
 
 	runConfiguredFaderChecks();
 }
+
+function selectStrip(input: number) {
+	selectedVMChannel = input;
+	setFLeds(("F" + (selectedVMChannel + 1).toString()) as ControlType);
+	setupVMFadeInputListener(FADER_TYPES.BUS, FADER_TYPES.STRIP, selectedVMChannel);
+	refreshFromVM();
+}
+
+// Listen for F keys for bus change
+function keyDownListener(key) {
+	if (key.action.startsWith("F")) {
+		const index = parseInt(key.action.substring(1)) - 1; // Extract index from F key
+		selectStrip(index);
+	}
+}
+
+let selectListener;
 
 function start() {
 	controller.right().setControlButton("Outputs", "SOLID");
 
 	vuMeterBusesTask(true);
 
-	setupVMFadeInputListener(FADER_TYPES.BUS, FADER_TYPES.BUS, 10);
+	selectListener = buildSendListener(() => selectedVMChannel, FADER_TYPES.BUS);
+
+	selectStrip(selectedVMChannel);
+	// setupVMFadeInputListener(FADER_TYPES.BUS, FADER_TYPES.STRIP, selectedVMChannel);
 
 	refreshFromVM();
 
+	controller.addListener("keyDown", keyDownListener);
+	controller.addListener("channelAction", selectListener);
 	controller.addListener("channelAction", muteChannelActionListener);
 	vmEventEmitter.addListener("change", refreshFromVM);
 }
@@ -39,6 +68,8 @@ function stop() {
 	vuMeterBusesTask(false);
 
 	takeDownVMFadeInputListener();
+	controller.removeListener("keyDown", keyDownListener);
+	controller.removeListener("channelAction", selectListener);
 	controller.removeListener("channelAction", muteChannelActionListener);
 	vmEventEmitter.removeListener("change", refreshFromVM);
 
